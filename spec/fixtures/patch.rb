33c2a05ea46eddb33d0bec81705e13369d89ef25
Jan Grodowski <jgrodowski@gmail.com>
2018-09-21 13:26:59 +0200

    update fixtures

---
+++ spec/fixtures/patch.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class BaconClass
+  def self.bar
+    puts "BaconClass\x87"
+  end
+
+  def foo
+    puts 'Covered line'
+    puts 'BaconClass#f'
+  end
+
+  class BaconChildClass
+    def foo_child
+      puts 'BaconClass::BaconChildClass#lol_im_uncovered'
+    end
+  end
+  private_constant :BaconChildClass
+end
